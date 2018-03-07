31027de26bed93a62337dcba6ec0a81487cb13a2
heyitskaya <ni23x@mtholyoke.edu>
2018-03-06 22:39:48 -0500

    ls, cd, pwd work however setPath is not working due to searchFile not properly generating full path, test with args of size 1 for now

--- whoosh.c
+++ whoosh.c
@@ -9,22 +9,24 @@ void reportError();
 void checkCommand();
 void pwd();
 void cd();
+void setPath();
 void exec(char *string);
 int parseCommand(char *string);
 int searchFile(char *file);
 
 // the maximun length of command line
 int const MAX_COMMAND = 128;
-
+//int numPaths;
 char *line;
 char **command;
 char **path;
-
+//TODO: figure out exact number of arguments in the command
 // number of words in a command
 int num_words= 128;
+int num_args=0;
 
 int main(int argc, char **argv){
-
+	num_args=argc;
 	if(argc > 1){
 		reportError();	
 		exit(1);
@@ -33,7 +35,7 @@ int main(int argc, char **argv){
 	line = (char*)malloc(sizeof(char) * MAX_COMMAND);
 	path = (char**)calloc( num_words, sizeof(char*) * MAX_COMMAND );
 	path[0] = "/bin";
-
+	//numPaths=1;
 	checkCommand();
 
 	return 0;
@@ -57,10 +59,10 @@ void checkCommand(){
 		command[0][(int)strlen(command[0])-1] = '\0';
 	}
 
-    if(strcmp(command[0], "exit") == 0){
-    	free(line);
-    	free(command);
-    	free(path);
+   	 if(strcmp(command[0], "exit") == 0){
+    		free(line);
+    		free(command);
+    		free(path);
 		exit(0);
 	}
 	// print working directory
@@ -71,14 +73,26 @@ void checkCommand(){
 	else if(strcmp(command[0], "cd") == 0){
 		cd();
 	}
-	else {
-		// if there exists such executable file, then execute it
-		if(searchFile(command[0]) == 1)
-			exec(command[0]);
-		else 
-			reportError();
+	else if(strcmp(command[0],"setpath") == 0){
+	//	if(num_args>2){
+			
+			setPath();
+	//	}
+	//	else{
+	//		return;
+	//	}
 	}
+	else{
+	//sth wrong with searchFile
+		printf("%s\n", command[0]);
+		if(searchFile(command[0]) == 1){
+			exec(command[0]);
+		}
+		else{
+			reportError();
+		}
 
+	}
 	free(command);
 
   	checkCommand();
@@ -113,6 +127,16 @@ void cd(){
 			reportError();
 		}
 	}	
+}
+
+void setPath(){
+	
+                 for(int i=0;i<1;i++){ //start with one path
+			path[i]=(char *)malloc(strlen(command[i+1])+1);
+                        strcpy(path[i], command[i+1]);
+			printf("The path is %s\n", path[i]);
+                 }
+
 }
 
 void exec(char *string){
@@ -158,21 +182,25 @@ int parseCommand(char *string){
 
    	return index;
 }
-
+//changed to search every path
 // return 1 if there exists the file in the current path
 int searchFile(char *file) {
+	printf("file is %s\n", file);
     int result = 0;
     struct stat buf;
-
-    char str[strlen(file)+strlen(path[0])+1];
-	strcpy(str, path[0]);
+    for(int i=0;i<1;i++){
+	printf("current path is %s\n",path[i]);
+   	char str[strlen(file)+strlen(path[i])+1];
+	strcpy(str, path[i]);
 	strcat(str, "/");
 	strcat(str, file);
-
-    if(stat(str, &buf) == 0)
-    	result = 1;
+	printf("full path is %s", str);
+    	if(stat(str, &buf) == 0){
+    		result = 1;
+    	}
 
     return result;
+}
 }
 
 void reportError() {
