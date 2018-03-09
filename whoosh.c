0f293db5d224bae1f06637aaace124944c6e90eb
Sujin Kim <kim53s@mtholyoke.edu>
2018-03-08 22:24:10 -0500

    fixxx

--- whoosh.c
+++ whoosh.c
@@ -11,88 +11,112 @@ void pwd();
 void cd();
 void setPath();
 void printPath();
-void exec(char *string);
+void exec(char *file, char *path);
 void execWithPath(char str[]);
 int parseCommand(char *string);
-int searchFile(char *file);
+int searchFile(char *file, char *path);
 
 // the maximun length of command line
 int const MAX_COMMAND = 128;
 char *line;
 char **command;
-char **path;
-//TODO: figure out exact number of arguments in the command
-// number of words in a command
-int num_words= 128;
+char **currentPath;
 int numPaths=1;
+
 int main(int argc, char **argv){
-	if(argc > 1){
+
+	if(argc > 1){ 
 		reportError();	
 		exit(1);
 	}
 
-	line = (char*)malloc(sizeof(char) * MAX_COMMAND);
-	path = (char**)calloc( num_words, sizeof(char*) * MAX_COMMAND );
-	path[0] = "/bin";
+	// stores user input
+	//line = (char*)malloc(sizeof(char) * MAX_COMMAND);
+	currentPath = (char**)calloc( MAX_COMMAND, sizeof(char*) * MAX_COMMAND );
+	// default path
+	currentPath[0] = "/bin";
+
 	checkCommand(argc);
 
 	return 0;
-	
 }
 
+// recursively get user input and check if it is a valid command
 void checkCommand(){
 
-	command = (char**)calloc( num_words, sizeof(char*) * MAX_COMMAND ); 
+	command = (char**)calloc( MAX_COMMAND, sizeof(char*) * MAX_COMMAND ); 
+	line = (char*)malloc(sizeof(char) * MAX_COMMAND);
+
 	printf("whoosh> ");
 
 	// get user input
-	fgets(line, 128, stdin);
+	fgets(line, MAX_COMMAND+1, stdin);
 	//free(line);
-	int i=parseCommand(line);
-	if(i>0){
-		numPaths=i;
-	}
-	if(command[0][(int)strlen(command[0])-1] == '\n'){
-		command[0][(int)strlen(command[0])-1] = '\0';
-	}
 
-   	 if(strcmp(command[0], "exit") == 0){
-    		free(line);
-    		free(command);
-    		free(path);
-		exit(0);
-	}
-	// print working directory
-	else if(strcmp(command[0], "pwd") == 0){
-		pwd();
-	}
-	// change directory
-	else if(strcmp(command[0], "cd") == 0){
-		cd();
-	}
-	else if(strcmp(command[0],"setpath") == 0){
-		if(numPaths>=1){
-			setPath();
-		}
-		else{
-			return;
-		}
-	}
-	else if(strcmp(command[0],"printpath") == 0){
-		printPath();
+	// if the input is over 128 bytes, then report error
+	if(strlen(line) >= 128){
+		printf("LINE: %s\n", line);
+		reportError();
 	}
 	else{
-		if(searchFile(command[0]) == 1){
-		//	exec(command[0]);
+		// number of words in the command line
+		int i = parseCommand(line);
+
+		// if the last character is the newline, then delete it
+		if(command[0][(int)strlen(command[0])-1] == '\n'){
+			command[0][(int)strlen(command[0])-1] = '\0';
+		}
+
+	   	 if(strcmp(command[0], "exit") == 0){
+	    	free(line);
+	    	free(command);
+	    	free(currentPath);
+			exit(0);
+		}
+		// print working directory
+		else if(strcmp(command[0], "pwd") == 0){
+			pwd();
+		}
+		// change directory
+		else if(strcmp(command[0], "cd") == 0){
+			cd();
+		}
+		else if(strcmp(command[0],"setpath") == 0){
+
+			if(i >= 1){
+				numPaths = i;
+
+				setPath();
+			}
+			else{ // if path is not passed
+				reportError();
+			}
+		}
+		else if(strcmp(command[0],"printpath") == 0){
+			printPath();
 		}
 		else{
-			reportError();
+			// search for executable program in the current path
+			int result = 0;
+			for(int i = 0; i < numPaths; i++){
+				// if there exists an executable program, then execute it
+				if(searchFile(command[0], currentPath[i]) == 1){
+					exec(command[0], currentPath[i]);
+					result++;
+					break;
+				}
+			}
+			if(result == 0){  // the file is not found in any path
+				printf("NO SUCH COMMAND\n");
+				reportError();
+			}
 		}
-
 	}
 	free(command);
-
+	free(line);
+	
   	checkCommand();
+
 }
 
 //print working directory
@@ -129,30 +153,30 @@ void cd(){
 void setPath(){
 	
     for(int i=0;i<numPaths;i++){ //start with one path
-	path[i]=(char *)malloc(strlen(command[i+1])+1);
-        strcpy(path[i], command[i+1]);
-    }
-    // if the last character is '\n', then delete it
-    if(path[0][(int)strlen(path[0])-1] == '\n'){
-		path[0][(int)strlen(path[0])-1] = '\0';
-	}
+ 		currentPath[i]=(char *)malloc(strlen(command[i+1])+1);
+       	strcpy(currentPath[i], command[i+1]);
 
+       	// if the last character is '\n', then delete it
+   		if(currentPath[i][(int)strlen(currentPath[i])-1] == '\n'){
+			currentPath[i][(int)strlen(currentPath[i])-1] = '\0';
+		}
+    }
 }
 
 void printPath(){
-	for(int i=0;i<numPaths;i++){
-		printf("%s\n", path[i]);
+	for(int i = 0; i < numPaths; i++){
+		printf("%s\n", currentPath[i]);
 	}
 }
 
-void exec(char *string){
+void exec(char *file, char *path){
 	int status;
     char *args[2];
 		
-	char str[strlen(string)+strlen(path[0])+1];
-	strcpy(str, path[0]);
+	char str[strlen(file)+strlen(path)+1];
+	strcpy(str, path);
 	strcat(str, "/");
-	strcat(str, string);
+	strcat(str, file);
 	args[0] = str;       
 	args[1] = NULL;          
 
@@ -184,35 +208,33 @@ int parseCommand(char *string){
    		if(token != NULL)
    			index++;
    	}
+
    	return index;
 }
 //changed to search every path
 // return 1 if there exists the file in the current path
-int searchFile(char *file) {
+int searchFile(char *file, char *path) {
     int result = 0;
     struct stat buf;
-//printf("numPaths %i\n", numPaths);
-    for(int i=0;i<numPaths;i++){
-   	char str[strlen(file)+strlen(path[i])+1];
-	//if the last character is a new line character delete it
-	if(path[i][(int)strlen(path[i])-1] == '\n'){
-		path[i][(int)strlen(path[i])-1] = '\0';
+   
+	char str[strlen(file)+strlen(path)+1];
+
+	//if the last character is a new line character, delete it
+	if(path[(int)strlen(path)-1] == '\n'){
+		path[(int)strlen(path)-1] = '\0';
 	}
-	strcpy(str,path[i]);
+
+	strcpy(str,path);
 	strcat(str, "/");
 	strcat(str, file);
-//	printf("filePath is %s\n", str);
-    	if(stat(str, &buf) == 0){
-    		result = 1;
-		execWithPath(str);
-    	}
-	
-    }
-	if(result == 0){
-		printf("NO SUCH PROGRAM\n");
+	    
+	if(stat(str, &buf) == 0){
+	    result = 1;
 	}
+
     return result;
 }
+
 void execWithPath(char str[]){
 	int status;
 	char *args[2];
@@ -228,8 +250,8 @@ void execWithPath(char str[]){
 	else{
 		reportError();
 	}
-
 }
+
 void reportError() {
 	char error_message[30] = "An error has occurred\n";
     write(STDERR_FILENO, error_message, strlen(error_message));
